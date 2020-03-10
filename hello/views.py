858e30b8c3384227818216e054a12d1704c9759e
zack <zackmcginnis@hotmail.com>
2020-03-09 22:24:38 -0700

    updates

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import os
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello! ' * times)
+
 
 
 def db(request):
